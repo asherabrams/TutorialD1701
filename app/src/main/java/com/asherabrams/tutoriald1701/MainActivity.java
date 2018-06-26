1c3759a3e9b9be118de6ab358c672981b04aa333
asherabrams <asher813@aol.com>
2018-06-26 06:40:12 -0700

    Initial commit

---
+++ app/src/main/java/com/asherabrams/tutoriald1701/MainActivity.java
@@ -0,0 +1,30 @@
+// Sends a string of text to a new activity.  This version works.  Date code is April 17.
+
+package com.asherabrams.tutoriald1701;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view){
+
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+
+}
