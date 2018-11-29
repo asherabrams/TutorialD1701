900c797f767f33dee59df995dddc7a2a0d6bb091
asherabrams <asher813@aol.com>
2018-11-29 04:48:14 -0800

    Added line to comments.

--- app/src/main/java/com/asherabrams/tutoriald1701/MainActivity.java
+++ app/src/main/java/com/asherabrams/tutoriald1701/MainActivity.java
@@ -1,13 +1,22 @@
 // Sends a string of text to a new activity.  This version works.  Date code is April 17.
 // 2018F26 12:20 edit:  Added this line of text to comments.
-
-package com.asherabrams.tutoriald1701;
+// 2018K29 04:36.  Added line to comments.
+// 2018K29 04:39.  Still another line added to comments.
+// 2018K29 04:42.  One more line added to comments.
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+
+import com.asherabrams.tutoriald1701.DisplayMessageActivity;
+import com.asherabrams.tutoriald1701.R;
+
+import android.content.Intent;
+        ?
+
+        package com.asherabrams.tutoriald1701;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
